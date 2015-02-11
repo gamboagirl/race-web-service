c2666bb18669702e7f24d1ca54fb538e4ef9e666
MMG <gamboa_girl@hotmail.com>
2015-02-10 20:51:05 -0800

    Adds races2run json and adds it to api.php

--- api.php
+++ api.php
@@ -5,6 +5,9 @@ if(isset($_REQUEST['cat']))
 {
 	switch($_REQUEST['cat'])
 	{
+        case "races2run":
+			include('data/races2run.js');
+			break;
 		case "fiction":
 			include('data/fiction.js');
 			break;
